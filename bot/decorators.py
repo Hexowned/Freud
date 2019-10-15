5c0453e5ab5d1c26a0ce51ec52f2ad5a9e087868
Nexowned <ianchurch206@gmail.com>
2019-10-15 05:50:28 -0700

    completing assigning various decorator functions and methods

--- bot/decorators.py
+++ bot/decorators.py
@@ -15,3 +15,173 @@ from bot.constants import ERROR_REPLIES, RedirectOutput
 from bot.utilities.checks import with_role_check, without_role_check
 
 log = logging.getLogger(__name__)
+
+
+class InChannelCheckFailure(CheckFailure):
+    """Raised when a check fails for a message being sent in a whitelisted channel"""
+
+    def __init__(self, *channels: int):
+        self.channels = channels
+        channels_str = ', '.join(f"<#{c_id}>" for c_id in channels)
+
+        super().__init__(
+            f"Sorry, but you may only use this command within {channels_str}.")
+
+
+def in_channel(*channels: int, bypass_roles: Container[int] = None) -> Callable:
+    """Checks that the message is in a whitelisted channel or optionally has a bypass role"""
+    def predicate(ctx: Context) -> bool:
+        """In-Channel checker predicate"""
+        if ctx.channel.id in channels:
+            log.debug(f"{ctx.author} tried to call the `{ctx.command.name}` command. "
+                      f"The command was used in a whitelisted channel.")
+            return True
+
+        if bypass_roles:
+            if any(r.id in bypass_roles for r in ctx.author.roles):
+                log.debug(f"{ctx.author} tried to call the '{ctx.command.name}' command. "
+                          f"The command was not used in a whitelisted channel, "
+                          f"but the author had a role to bypass the in_channel check.")
+                return True
+
+        log.debug(f"{ctx.author} tried to call the '{ctx.command.name}' command. "
+                  f"The in_channel check failed.")
+
+        raise InChannelCheckFailure(*channels)
+
+    return commands.check(predicate)
+
+
+def with_role(*role_ids: int) -> Callable:
+    """Returns True if the user has any one of the roles in role_ids"""
+    async def predicate(ctx: Context) -> bool:
+        """With role checker predicate"""
+        return with_role_check(ctx, *role_ids)
+    return commands.check(predicate)
+
+
+def without_role(*role_ids: int) -> Callable:
+    """Returns True if the user does not have any of the roles in role_ids"""
+    async def predicate(ctx: Context) -> bool:
+        return without_role_check(ctx, *role_ids)
+    return commands.check(predicate)
+
+
+def locked() -> Callable:
+    """
+    Allows the user to only run one instance of the decorated command at a time.
+    Subsequent calls to the command from the same author are ignored until the command has completed invocation
+    This decorator must go before (below) the `command` decorator
+    """
+    def wrap(func: Callable) -> Callable:
+        func.__locks = WeakValueDictionary()
+
+        @wraps(func)
+        async def inner(self: Cog, ctx: Context, *args, **kwargs) -> None:
+            lock = func.__locks.setdefault(ctx.author.id, Lock())
+            if lock.locked():
+                embed = Embed()
+                embed.colour = Colour.red()
+
+                log.debug(f"User tried to invoke a locked command.")
+                embed.description = (
+                    "You're already using this command. Please wait until it is done before you use it again."
+                )
+                embed.title = random.choice(ERROR_REPLIES)
+                await ctx.send(embed=embed)
+                return
+            async with func.__locks.setdefault(ctx.author.id, Lock()):
+                await func(self, ctx, *args, **kwargs)
+        return inner
+    return wrap
+
+
+def redirect_output(destination_channel: int, bypass_roles: Container[int] = None) -> Callable:
+    """
+    Changes the channel in the context of the command to redirect the output to a certain channel
+    Redirect is bypassed if the author has a role to bypass redirection
+    This decorator must go before (below) the `command` decorator
+    """
+    def wrap(func: Callable) -> Callable:
+        @wraps(func)
+        async def inner(self: Cog, ctx: Context, *args, **kwargs) -> None:
+            if ctx.channel.id == destination_channel:
+                log.trace(
+                    f"Command {ctx.command.name} was invoked in destination_channel, not redirecting")
+                await func(self, ctx, *args, **kwargs)
+                return
+
+            if bypass_roles and any(role.id in bypass_roles for role in ctx.author.roles):
+                log.trace(
+                    f"{ctx.author} has role to bypass output redirection")
+                await func(self, ctx, *args, **kwargs)
+                return
+
+            redirect_channel = ctx.guild.get_channel(destination_channel)
+            old_channel = ctx.channel
+
+            log.trace(
+                f"Redirecting output of {ctx.author}'s command '{ctx.command.name}' to {redirect_channel.name}")
+            ctx.channel = redirect_channel
+            await ctx.channel.send(f"Here's the output of your command, {ctx.author.mention}")
+            await func(self, ctx, *args, **kwargs)
+
+            message = await old_channel.send(
+                f"Hey, {ctx.author.mention}, you can find the output of your command here "
+                f"{redirect_channel.mention}"
+            )
+
+            if RedirectOutput.delete_invocation:
+                await sleep(RedirectOutput.delete_delay)
+
+                with suppress(NotFound):
+                    await message.deleate()
+                    log.trace(
+                        "Redirect output: Deleted user redirection message")
+
+                with suppress(NotFound):
+                    await ctx.message.delete()
+                    log.trace("Redirect output: Deleted invocation message")
+        return inner
+    return wrap
+
+
+def respect_role_hierarchy(rarget_arg: Union[int, str] = 0) -> Callable:
+    """
+    Ensure the highest role of the invoking member is greater than that of the target member.
+    If the condition fails, a warning is sent tot he invoking context. A target which is not an
+    instance of discord.Member will always pass.
+    """
+    def wrap(func: Callable) -> Callable:
+        @wraps(func)
+        async def inner(self: COg, ctx: Context, *args, **kwargs) -> None:
+            try:
+                target = kwargs[target_arg]
+            except KeyError:
+                try:
+                    target = args[target_arg]
+                except IndexError:
+                    raise ValueError(
+                        f"Could not find target argument at position {target_arg!r}")
+
+            if not isinstancetarget, Member):
+                log.trace(
+                    "The target is not a discord.Member; skipping role hierarchy check.")
+                await func(self, ctx, *args, **kwargs)
+                return
+
+            cmd=ctx.command.name
+            actor=ctx.author
+            if target.top_role >= actor.top_role:
+                log.info(
+                    f"{actor} ({actor.id}) attempted to {cmd} "
+                    f"{target} ({target.id}), who has an equal or higher top role.""
+                )
+                await ctx.send(
+                    f":x: {actor.mention}, you may not {cmd} "
+                    "someone with an equal or highest role."
+                )
+            else:
+                await func(self, ctx, *args, **kwargs)
+            return inner
+        return wrap
---
+++ bot/utilities/scheduling.py
@@ -0,0 +1,64 @@
+import asyncio
+import contextlib
+import logging
+from abc import abstractmethod
+from typing import Coroutine, Dict, Union
+
+# from bot.utilities import CogABCMeta
+
+log = logging.getLogger(__name__)
+
+
+class Scheduler(metaclass=CogABCMeta):
+    """Task scheduler"""
+
+    def __init__(self):
+
+        self.cog_name = self.__class__.__name__
+        self.scheduled_task: Dict[str, asyncio.Task] = {}
+
+    @abstractmethod
+    async def _scheduled_task(self, task_object: dict) -> None:
+        """
+        A coroutine which handles the scheduling.
+        This is added to the scheduled task, and should wait the task duration, execute the desired
+        code, and then clean up the task.
+        """
+
+    def schedule_task(self, loop: asyncio.AbstractEventLoop, task_id: str, task_data: dict) -> None:
+        """Schedules a task"""
+        if task_id in self.scheduled_task:
+            return
+
+        task: asyncio.Task = create_task(loop, self._scheduled_task(task_data))
+
+        self.scheduled_task[task_id] = task
+
+    def cancel_task(self, task_id: str) -> None:
+        """Un-schedules a task"""
+        task = self.scheduled_task.get(task_id)
+
+        if task is None:
+            log.warning(
+                f"{self.cog_name}: Failed to unschedule {task_id} (no task found).")
+            return
+
+        task.cancel()
+        log.debug(f"{self.cog_name}: Unscheduled {task_id}.")
+        del self.scheduled_task[task_id]
+
+
+def create_task(loop: asyncio.AbstractEventLoop, coro_or_future: Union[Coroutine, asyncio.Future])
+
+"""Creates an asyncio.Task object from a coroutine or future object"""
+task: asyncio.Task = asyncio.ensure_future(coro_or_future, loop=loop)
+
+# Silently ignore exceptions in a callback (handles the CancelledError nonsense)
+task.add_done_callback(_silent_exception)
+return task
+
+
+def _silent_exception(future: asyncio.Future) -> None:
+    """Supress future's exceptions"""
+    with contextlib.suppress(Exception):
+        future.exception()
